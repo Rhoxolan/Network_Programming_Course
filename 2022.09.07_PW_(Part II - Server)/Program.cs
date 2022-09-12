d225b0517529887a6c680b7b46e4b1a72e6eb381
Rhoxolan <pba17mail@gmail.com>
2022-09-12 21:03:31 +0300

    Add to PW

---
+++ 2022.09.07_PW_(Part II - Server)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2022._09._07_PW__Part_II___Server_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
