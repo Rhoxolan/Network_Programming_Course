a0f5ecbb07ce70e4ae3d9ee44b2ef806e756ecc9
Rhoxolan <pba17mail@gmail.com>
2022-09-13 10:14:31 +0300

    Add to PW

--- 2022.09.07_PW_2_(Part II - Server)/ServerApplication.cs
+++ 2022.09.07_PW_2_(Part II - Server)/ServerApplication.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Net.WebSockets;
 
 namespace _2022._09._07_PW_2__Part_II___Server_
 {
@@ -19,47 +20,48 @@ namespace _2022._09._07_PW_2__Part_II___Server_
             while (true)
             {
                 Socket newSocket = listeningSocket.Accept();
-                try
+                Task.Run(() => Answer(newSocket));
+            }
+        }
+
+        private void Answer(Socket socket)
+        {
+            try
+            {
+                StringBuilder builder = new();
+                byte[] buff = new byte[1024];
+                int length = 0;
+                do
                 {
-                    StringBuilder builder = new();
-                    byte[] buff = new byte[1024];
-                    int length = 0;
-                    do
-                    {
-                        length = newSocket.Receive(buff);
-                        string str = Encoding.Default.GetString(buff, 0, length);
-                        builder.Append(str);
-                    }
-                    while (newSocket.Available > 0);
-                    string val = builder.ToString();
-                    if(val == "D")
-                    {
-                        string message = $"{DateTime.Now.ToLongDateString()}";
-                        buff = Encoding.Default.GetBytes(message);
-                        newSocket.Send(buff);
-                    }
-                    else if(val == "T")
-                    {
-                        string message = $"{DateTime.Now.ToShortTimeString()}";
-                        buff = Encoding.Default.GetBytes(message);
-                        newSocket.Send(buff);
-                    }
-                    else if (val != "T" && val != "D")
-                    {
-                        string message = $"Неизвестный запрос '{val}'";
-                        buff = Encoding.Default.GetBytes(message);
-                        newSocket.Send(buff);
-                    }
+                    length = socket.Receive(buff);
+                    string str = Encoding.Default.GetString(buff, 0, length);
+                    builder.Append(str);
                 }
-                catch (Exception ex)
+                while (socket.Available > 0);
+                string val = builder.ToString();
+                if (val == "D")
                 {
-                    Console.WriteLine(ex.Message);
+                    string message = $"{DateTime.Now.ToLongDateString()}";
+                    buff = Encoding.Default.GetBytes(message);
+                    socket.Send(buff);
                 }
-                finally
+                else if (val == "T")
                 {
-                    newSocket.Shutdown(SocketShutdown.Both);
-                    newSocket.Close();
+                    string message = $"{DateTime.Now.ToShortTimeString()}";
+                    buff = Encoding.Default.GetBytes(message);
+                    socket.Send(buff);
                 }
+                else if (val != "T" && val != "D")
+                {
+                    string message = $"Неизвестный запрос '{val}'";
+                    buff = Encoding.Default.GetBytes(message);
+                    socket.Send(buff);
+                }
+            }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
             }
         }
 
