63d4339f0b251c75f3de41112c49b57672067723
Rhoxolan <pba17mail@gmail.com>
2022-09-17 20:57:37 +0300

    Add PW

---
+++ 2022.09.12_PW_(Part I - Server)/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _2022._09._12_PW__Part_I___Server_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ServerApplication serverApplication = new();
+            serverApplication.Start();
+        }
+    }
+}
---
+++ 2022.09.12_PW_(Part I - Server)/ServerApplication.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace _2022._09._12_PW__Part_I___Server_
+{
+    public class ServerApplication
+    {
+        public void Start() => Program();
+
+        private void Program()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            IPAddress address = IPAddress.Parse("192.168.43.255");
+            EndPoint remoteEP = new IPEndPoint(address, 5000);
+        }
+
+        private void AddToLog(string str)
+        {
+            FileStream fs = new(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "serverLogs.log"), FileMode.Append);
+            using StreamWriter sw = new(fs);
+            sw.WriteLine(str);
+        }
+
+        private void ExitWait()
+        {
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.End)
+                {
+                    AddToLog($"{DateTime.Now}: Выключение сервера");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
