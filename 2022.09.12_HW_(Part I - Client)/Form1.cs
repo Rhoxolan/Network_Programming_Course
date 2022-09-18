ba3ef4de55d524c0c69c00f31c1a454347ffcfc2
Rhoxolan <pba17mail@gmail.com>
2022-09-18 21:26:29 +0300

    Add to HW

--- 2022.09.12_HW_(Part I - Client)/Form1.cs
+++ 2022.09.12_HW_(Part I - Client)/Form1.cs
@@ -21,23 +21,9 @@ namespace _2022._09._12_HW__Part_I___Client_
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string message = "GET_PRODUCTS";
-            byte[] buff = Encoding.Default.GetBytes(message);
-            socket.SendTo(buff, iPEndPoint);
-
-            Socket receiveSocket = socket;
-            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 3025);
-            buff = new byte[2048];
-            int len = receiveSocket.ReceiveFrom(buff, ref remoteEP);
-            List<string> productList = JsonSerializer.Deserialize<List<string>>(Encoding.Default.GetString(buff, 0, len))!;
-            listBox1.DataSource = productList;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (listBox1.SelectedIndex != -1)
+            try
             {
-                string message = (string)listBox1.SelectedItem;
+                string message = "GET_PRODUCTS";
                 byte[] buff = Encoding.Default.GetBytes(message);
                 socket.SendTo(buff, iPEndPoint);
 
@@ -45,8 +31,36 @@ namespace _2022._09._12_HW__Part_I___Client_
                 EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 3025);
                 buff = new byte[2048];
                 int len = receiveSocket.ReceiveFrom(buff, ref remoteEP);
-                int price = JsonSerializer.Deserialize<int>(Encoding.Default.GetString(buff, 0, len))!;
-                textBox1.Text = price.ToString();
+                List<string> productList = JsonSerializer.Deserialize<List<string>>(Encoding.Default.GetString(buff, 0, len))!;
+                listBox1.DataSource = productList;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                try
+                {
+                    string message = (string)listBox1.SelectedItem;
+                    byte[] buff = Encoding.Default.GetBytes(message);
+                    socket.SendTo(buff, iPEndPoint);
+
+                    Socket receiveSocket = socket;
+                    EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 3025);
+                    buff = new byte[2048];
+                    int len = receiveSocket.ReceiveFrom(buff, ref remoteEP);
+                    int price = JsonSerializer.Deserialize<int>(Encoding.Default.GetString(buff, 0, len))!;
+                    textBox1.Text = price.ToString();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
     }
--- 2022.09.12_HW_(Part II - Server)/ServerApplication.cs
+++ 2022.09.12_HW_(Part II - Server)/ServerApplication.cs
@@ -18,37 +18,47 @@ namespace _2022._09._12_HW__Part_II___Server_
 
         private void Program()
         {
-            AddToLog($"{DateTime.Now}: Запуск сервера.");
-            Task.Run(ExitWait);
-            IPAddress serverAddress = IPAddress.Loopback;
-            IPEndPoint serverEP = new(serverAddress, 3025);
-            Socket listeningSocket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            listeningSocket.Bind(serverEP);
-
-            Socket receiveSocket = listeningSocket;
-            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 3025);
-            do
+            try
             {
-                byte[] buff = new byte[2048];
-                int len = receiveSocket.ReceiveFrom(buff, ref remoteEP);
-                string message = Encoding.Default.GetString(buff, 0, len);
-                AddToLog($"{DateTime.Now}: От {remoteEP} получено следующее сообщение: {message}");
-                if (message == "GET_PRODUCTS")
+                AddToLog($"{DateTime.Now}: Запуск сервера.");
+                Task.Run(ExitWait);
+                IPAddress serverAddress = IPAddress.Loopback;
+                IPEndPoint serverEP = new(serverAddress, 3025);
+                Socket listeningSocket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                listeningSocket.Bind(serverEP);
+
+                Socket receiveSocket = listeningSocket;
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 3025);
+                do
                 {
-                    List<string> names = new() { products[0].Name, products[1].Name, products[2].Name };
-                    buff = Encoding.Default.GetBytes(JsonSerializer.Serialize(names));
-                    receiveSocket.SendTo(buff, remoteEP);
+                    byte[] buff = new byte[2048];
+                    int len = receiveSocket.ReceiveFrom(buff, ref remoteEP);
+                    string message = Encoding.Default.GetString(buff, 0, len);
+                    if (message == "GET_PRODUCTS")
+                    {
+                        AddToLog($"{DateTime.Now}: От {remoteEP} получен запрос на предоставление списка товаров");
+                        List<string> names = new() { products[0].Name, products[1].Name, products[2].Name };
+                        buff = Encoding.Default.GetBytes(JsonSerializer.Serialize(names));
+                        receiveSocket.SendTo(buff, remoteEP);
+                        AddToLog($"{DateTime.Now}: К {remoteEP} отправлена информация о списке товаров");
+                    }
+                    if ((from p in products where p.Name == message select p.Name).Any())
+                    {
+                        AddToLog($"{DateTime.Now}: От {remoteEP} получен запрос на предоставление цены товара {message}");
+                        int price = (from p in products
+                                     where p.Name == message
+                                     select p.Price).Single();
+                        buff = Encoding.Default.GetBytes(JsonSerializer.Serialize(price));
+                        receiveSocket.SendTo(buff, remoteEP);
+                        AddToLog($"{DateTime.Now}: К {remoteEP} отправлена информация о цене товара {message}");
+                    }
                 }
-                if ((from p in products where p.Name == message select p.Name).Any())
-                {
-                    int price = (from p in products
-                                 where p.Name == message
-                                 select p.Price).Single();
-                    buff = Encoding.Default.GetBytes(JsonSerializer.Serialize(price));
-                    receiveSocket.SendTo(buff, remoteEP);
-                }
+                while (true);
             }
-            while (true);
+            catch(Exception  ex)
+            {
+                AddToLog($"{DateTime.Now}: Вызвано исключение: {ex.Message}. Работа сервера прекращена.");
+            }
         }
 
         private void AddToLog(string str)
