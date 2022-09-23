bfe53a37eff793e42dcc62b5fe33a3605a7f41b0
Rhoxolan <pba17mail@gmail.com>
2022-09-23 20:57:17 +0300

    Add PW

---
+++ 2022.09.19_PW_(Part I - Server)/Form1.cs
@@ -0,0 +1,25 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _2022._09._19_PW__Part_I___Server_
+{
+    public partial class Form1 : Form
+    {
+        UdpClient udpServer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            udpServer = new(new IPEndPoint(IPAddress.Loopback, 9000));
+            udpServer.JoinMulticastGroup(IPAddress.Parse("224.5.5.0"), 1); //Админы
+            udpServer.JoinMulticastGroup(IPAddress.Parse("224.5.5.1"), 1); //Техподдержка
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(textBox1.Text);
+            udpServer.Send(data, new IPEndPoint(IPAddress.Parse("224.5.5.0"), 7500)); //Разобраться с ошибкой
+        }
+    }
+}
---
+++ 2022.09.19_PW_(Part I - Server)/Program.cs
@@ -0,0 +1,17 @@
+namespace _2022._09._19_PW__Part_I___Server_
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
