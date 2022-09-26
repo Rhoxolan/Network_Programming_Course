c8bc7f263d125357a820c84679137f8525ad98d8
Rhoxolan <pba17mail@gmail.com>
2022-09-26 13:36:12 +0300

    Add PW

---
+++ 2022.09.21_PW/Form1.cs
@@ -0,0 +1,15 @@
+namespace _2022._09._21_PW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
