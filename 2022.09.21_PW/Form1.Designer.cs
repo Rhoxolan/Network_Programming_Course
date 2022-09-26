7b681f2380308ac3052230aecd18c6f7ad2b15e3
Rhoxolan <pba17mail@gmail.com>
2022-09-26 23:18:08 +0300

    Add to PW

--- 2022.09.21_PW/Form1.Designer.cs
+++ 2022.09.21_PW/Form1.Designer.cs
@@ -29,31 +29,101 @@
         private void InitializeComponent()
         {
             this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // cartesianChart1
             // 
-            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 104);
             this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(776, 426);
+            this.cartesianChart1.Size = new System.Drawing.Size(844, 411);
             this.cartesianChart1.TabIndex = 0;
             this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "lat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "lon";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(12, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(156, 23);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(868, 527);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cartesianChart1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Погода";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
     }
 }
--- 2022.09.21_PW/Form1.cs
+++ 2022.09.21_PW/Form1.cs
@@ -1,15 +1,67 @@
+using LiveCharts.WinForms;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Definitions.Charts;
+using LiveCharts.Defaults;
+using System.Text.Json;
+using System.Windows.Media;
+using Timer = System.Windows.Forms.Timer;
+using Microsoft.VisualBasic.Logging;
+
 namespace _2022._09._21_PW
 {
     public partial class Form1 : Form
     {
+        Timer timer;
+
         public Form1()
         {
             InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 10800000;
+            timer.Tick += (s, e) => DataLoader();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            textBox1.Text = "47.908091";
+            textBox2.Text = "33.387017";
+            DataLoader();
+            timer.Start();
         }
+
+
+        private async void DataLoader()
+        {
+            try
+            {
+                using HttpClient client = new();
+                using HttpRequestMessage httpRequest = new(HttpMethod.Get,
+                    $"https://api.openweathermap.org/data/2.5/forecast?lat={textBox1.Text}&lon={textBox2.Text}&units=metric&appid=435d51d8ba48fa0491ed9483578a3495");
+                HttpResponseMessage httpResponse = await client.SendAsync(httpRequest);
+                MyWeather myWeather = JsonSerializer.Deserialize<MyWeather>(await httpResponse.Content.ReadAsStringAsync())!;
+
+                cartesianChart1.AxisX.Clear();
+                cartesianChart1.AxisX.Add(new Axis()
+                {
+                    Labels = myWeather.list.Select(l => DateTime.Parse(l.dt_txt).ToLocalTime().ToString()).ToArray()
+                });
+
+                cartesianChart1.Series = new SeriesCollection()
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double>(myWeather.list.Select(l => l.main.temp))
+                },
+            };
+                textBox3.Text = myWeather.city.name;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) => DataLoader();
     }
 }
