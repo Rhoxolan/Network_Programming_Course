4a8ac643a2499eb527adb29f47463f49b8a9b6cf
Rhoxolan <pba17mail@gmail.com>
2022-09-27 23:50:02 +0300

    Add to PW

--- 2022.09.23_PW/Form1.Designer.cs
+++ 2022.09.23_PW/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -98,11 +100,30 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отправить результаты поиска";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 330);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(351, 23);
+            this.textBox3.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 398);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.numericUpDown1);
@@ -125,5 +146,7 @@
         private NumericUpDown numericUpDown1;
         private TextBox textBox2;
         private Button button1;
+        private Button button2;
+        private TextBox textBox3;
     }
 }
--- 2022.09.23_PW/Form1.cs
+++ 2022.09.23_PW/Form1.cs
@@ -1,21 +1,80 @@
+using System.Text;
 using System.Text.Json;
+using MailKit;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Configuration;
 
 namespace _2022._09._23_PW
 {
     public partial class Form1 : Form
     {
+        StringBuilder searchResponseStringBuilder;
+
         public Form1()
         {
             InitializeComponent();
+            searchResponseStringBuilder = new();
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            using HttpClient client = new();
-            using HttpRequestMessage httpRequest = new(HttpMethod.Get,
-                $"");
-            HttpResponseMessage httpResponse = await client.SendAsync(httpRequest);
-            MyWeather myWeather = JsonSerializer.Deserialize<MyWeather>(await httpResponse.Content.ReadAsStringAsync())!;
+            try
+            {
+                using HttpClient client = new();
+                using HttpRequestMessage httpRequest = new(HttpMethod.Get,
+                    $"http://www.omdbapi.com/?apikey=key&t={textBox1.Text}&y={textBox2.Text}");
+                HttpResponseMessage httpResponse = await client.SendAsync(httpRequest);
+                Movie mov = JsonSerializer.Deserialize<Movie>(await httpResponse.Content.ReadAsStringAsync())!;
+
+                searchResponseStringBuilder.Clear();
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Title)}: {mov.Title}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Year)}: {mov.Year}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Director)}: {mov.Director}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Actors)}: {mov.Actors}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Released)}: {mov.Released}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Genre)}: {mov.Genre}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.Awards)}: {mov.Awards}");
+                searchResponseStringBuilder.AppendLine($"{nameof(mov.DVD)}: {mov.DVD}");
+
+                textBox2.Text = searchResponseStringBuilder.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (searchResponseStringBuilder.Length > 0)
+            {
+                try
+                {
+                    using (FileStream fs = new("SearchAnswer.txt", FileMode.Create))
+                    {
+                        using StreamWriter sw = new(fs);
+                        sw.Write(searchResponseStringBuilder.ToString());
+                    }
+
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress("Name", "example@example.com"));
+                    message.To.Add(new MailboxAddress("Name", textBox3.Text));
+                    message.Subject = "Результаты поиска";
+                    BodyBuilder builder = new();
+                    await builder.Attachments.AddAsync("SearchAnswer.txt", File.OpenRead("SearchAnswer.txt"), new ContentType("text", "txt"));
+                    message.Body = builder.ToMessageBody();
+                    using var client = new SmtpClient();
+                    await client.ConnectAsync("smtp.example.com", 465, true);
+                    await client.AuthenticateAsync("example@example.com", "password");
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
